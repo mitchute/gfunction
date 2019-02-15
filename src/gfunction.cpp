b327e1d6888785eb8c2256a447abb79f772edbbc
Matt Mitchell <matt.s.mitchell@okstate.edu>
2019-02-15 14:50:37 -0600

    initial commit

---
+++ src/gfunction.cpp
@@ -0,0 +1,5 @@
+class gfunction
+{
+public:
+		int a;
+};
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+
+# include "../third_party/nlohmann/json.hpp"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!";
+    return 0;
+}
