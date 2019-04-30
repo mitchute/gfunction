49db7439238192310e000cb8eeb89087b466dfc5
Matt Mitchell <matt.s.mitchell@okstate.edu>
2019-04-30 16:51:32 -0500

    bump nlohmann/json.hpp. read input file

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
+// C++ headers
 # include <iostream>
+# include <fstream>
+# include <sstream>
 
+// project headers
 # include "../third_party/nlohmann/json.hpp"
 
+// namespaces
 using namespace std;
 
-int main()
+// shortcuts
+using json = nlohmann::json;
+
+int main(int argc, char *argv[])
 {
-    cout << "Hello, World!";
+	// read input file
+	std::string ifPath = argv[1];
+	std::ifstream t(ifPath);
+	std::stringstream buffer;
+	buffer << t.rdbuf();
+
+	// read json input data
+	auto j = json::parse(buffer.str());
+	
+    cout << j["answer"]["everything"];
     return 0;
 }
